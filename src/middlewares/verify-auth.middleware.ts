080548f24edc59ef4f7dbdc6e3b1d2b1f578a3fb
devatassimilate <developer@assimilatetechnologies.com>
2022-09-26 15:24:35 +0530

    Updates

--- src/middlewares/verify-auth.middleware.ts
+++ src/middlewares/verify-auth.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import passport from 'passport';
 import { NotAuthorizedError } from '../errors/not-authorized.error';
 
-const verifyAuth = (req: Request, res: Response, next: NextFunction) =>
+export const verifyAuth = (req: Request, res: Response, next: NextFunction) =>
     passport.authenticate('jwt', { session: false }, (err: any, user: any) => {
         if (err || !user) {
             throw new NotAuthorizedError(err?.message ?? 'Unauthorized!');
@@ -11,5 +11,3 @@ const verifyAuth = (req: Request, res: Response, next: NextFunction) =>
         req.user = user;
         return next();
     })(req, res, next);
-
-export default verifyAuth;
